f362a8808a8f501d94edcbc75b3d84589cf4adc5
Anurag-Tarai <anuragtarai051@gmail.com>
2024-11-06 20:57:09 +0530

    Started working on Home page, searching and projects list page!

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProjectList from '../project-list/ProjectList'
+
+const Home = () => {
+  return (
+    <div>
+        <ProjectList/>
+    </div>
+  )
+}
+
+export default Home
